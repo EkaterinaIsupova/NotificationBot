7d26c7e16b6c0a0f4a072853ce35629f56ff4ab5
EkaterinaIsupova <153060579+EkaterinaIsupova@users.noreply.github.com>
2024-04-24 23:17:42 +0300

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -1,7 +1,31 @@
 def format_excel_file(file_id):
     # Заглушка для функции обработки файла
     # Сюда будет вставлена реальная функция обработки файла
-    pass
+    task = file_id['task']
+  if not isinstance(task, str):
+    raise TypeError("поле 'task' должно быть типа str")
+  
+  descr = file_id['description']
+  if not isinstance(descr, str):
+    raise TypeError("поле 'description' должно быть типа int")
+  
+  date_time_obj = datetime.datetime.strptime(str(file_id['date/time']), '%Y-%m-%d %H:%M:%S')
+  if not isinstance(date_time_obj, datetime.date):
+    raise TypeError("поле 'date_time_obj' должно быть типа datetime")
+  
+  deadline = int(file_id['time_before_the_deadline'])
+  if not isinstance(deadline, int):
+    raise TypeError("поле 'time_before_the_deadline' должно быть типа int")
+  if deadline <= 0:
+    raise ValueError("В поле 'time_before_the_deadline' должно храниться положительное число")
+  
+  for sentence in file_id['receiverse']: 
+    words = sentence.split()
+  particip = file_id['receiverse'].split()
+  if not isinstance(particip, list):
+    raise TypeError("поле 'receiverse' должно быть типа list")
+  
+  return {'task': task, 'description': descr, 'date/time': date_time_obj, 'time_before_the_deadline': deadline, 'receiverse': particip}
 
 
 def get_event_list_for_user(user_tag) -> dict | None:
